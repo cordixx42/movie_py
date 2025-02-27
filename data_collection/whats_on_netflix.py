bb1004b0068f16759e781500a78b3d88a13d5bff
Elias Miller <emiller@cert.org>
2025-02-27 15:57:56 -0500

    New data and new WON scrape.

--- data_collection/whats_on_netflix.py
+++ data_collection/whats_on_netflix.py
@@ -1,82 +1,92 @@
-'''
-Data Focused Python - Class Project
-
-Script which scrapes movie info from whats-on-netflix.com/library/movies
-minor issue is that the films don't have genre tags and this takes forever. 
-
-'''
-from bs4 import BeautifulSoup as bs4
+import time
 import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-from datetime import date
 
-# takes url 
-# return dataframe
+# takes URL to the WON site. Returns a dataframe of all movies found in the table on the site. 
+# this may need to be changed if the site changes. 
+# developed and tested on windows with Spyder - Seems that there is may be ad overlays which may get in the way on other systems. 
+# each page loads for about 10 seconds so this is quite a long running script. If your internet is faster, this may be able to run with less wait time. 
+# as of 02-27-2025 there are 190 pages of tables. Results in 4748 rows of movies. Takes about 30 minutes to run. 
+
 def wonScrape(url):
-    # use chrome to get webpage
+    # Set up Selenium WebDriver (You must make sure that chromedriver is in your PATH)
     driver = webdriver.Chrome()
     driver.get(url)
-
-    # lists of movie values initialization
+   
+    # Initialize lists to hold movie data
     title_lst = []
-    languages_lst = []
-    release_lst = []
+    language_lst = []
+    release_year_lst = []
     rating_lst = []
-    imdb_lst = []
-
+    imdb_score_lst = []
+   
     while True:
-        # wait for table element to load
+        # Wait until the table body is populated with data
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#netflixlist tbody tr td img')))
-        # data sometimes takes a extra bit of time to load 
+   
+        # Give an extra moment for the data to fully load
         time.sleep(5)
-        # get page and find the table tbody
-        soup = bs4(driver.page_source, 'html.parser')
+   
+        # Get the page source and parse it
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+   
+        # Find the tbody containing the movie list and loop through each row in the tbody and extract metadata
         tbody = soup.find('tbody')
 
-        # loop through the table of one page - could probably make this another function if we wanted to. 
-        for row in tbody.find_all('tr'):  # tr elements specify table rows. They alternate between 'even' and 'odd'
-            columns = row.find_all('td') # td elements mark table data
-            if len(columns)>=7: # table is a size of 7 columns
+        for row in tbody.find_all('tr'):
+            columns = row.find_all('td')
+           
+            if len(columns) >= 7:
                 title = columns[2].text.strip()
                 language = columns[3].text.strip()
                 release_year = columns[4].text.strip()
                 rating = columns[5].text.strip()
                 imdb_score = columns[6].text.strip()
-                # add values to list
+               
                 title_lst.append(title)
-                languages_lst.append(language)
-                release_lst.append(release_year)
+                language_lst.append(language)
+                release_year_lst.append(release_year)
                 rating_lst.append(rating)
-                imdb_lst.append(imdb_score)
+                imdb_score_lst.append(imdb_score)
+   
+        # Check if there is a next page and access it.
         try:
-            # try to find the button on the bottom right of the table to go to the next page - 25 rows per page >4000 total rows
-            next_page = driver.find_element(By.ID, 'netflixlist_next')
-            if 'disabled' not in next_page.get_attribute('class'):
-                next_page.click()
-                # Need extra wait otherwise does not get past page 45
-                WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#netflixlist tbody tr td img')))
+            next_button = driver.find_element(By.ID, 'netflixlist_next')
+            # Wait for any iframe/ad to disappear or be invisible before attempting click
+            WebDriverWait(driver, 20).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, 'iframe')))
+            if 'disabled' not in next_button.get_attribute('class'):
+                try:
+                    # try scrolling away from the ad
+                    driver.execute_script("window.scrollBy(0, 300);")
+                    next_button.click()
+                    # Wait for the next page to load (was a problem for a while. Makes everything take forever.)
+                    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#netflixlist tbody tr td img')))
+                except:
+                    # Try using JavaScript if normal click fails
+                    driver.execute_script("arguments[0].click();", next_button)  
+                    # Wait for the next page to load (was a problem for a while. Makes everything take forever.)
+                    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#netflixlist tbody tr td img')))
             else:
-                # no more pages 
-                break
-        except ValueError as e:
+                break  # No more pages
+        except Exception as e:
             print('Error, either no more pages or webpage elements changed: ', e)
-        # extra few seconds for loading - may have needed due to bad connection. 
+   
+        # Give an extra moment for the next page to load - need to test if this is needed.
         time.sleep(5)
+   
     # out of loop - done with scrape, close driver    
     driver.quit()
-    movies_df = pd.DataFrame({'Title': title_lst, 'Language': languages_lst, 'Release':release_lst, 'Rating':rating_lst, 'IMDB':imdb_lst})
+    movies_df = pd.DataFrame({'Title': title_lst, 'Language': language_lst, 'Release':release_year_lst, 'Rating':rating_lst, 'IMDB':imdb_score_lst})
     return movies_df
 
 def main():
     url = 'https://www.whats-on-netflix.com/library/movies'
     df = wonScrape(url)
-    today = date.today()
-    date = today.strftime('%Y%m%d')
-    df.to_json(f'whatsOnNetflix{date}.json')
-    
+    df.to_json('whatsOnNetflix.json')
+   
 if __name__ == '__main__':
     main()
