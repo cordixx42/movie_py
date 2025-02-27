667600d1621a943547a98cf5bb390907f786fe3f
Hanna Cui <hannacui@Hannas-MacBook-Air-4.local>
2025-02-27 15:33:55 -0500

    documentation

--- cinepy/movierecommendation/views.py
+++ cinepy/movierecommendation/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from .likeXbutY import likeXbutY
 
 
+# dict of all languages which can be selected from ISO code to full language name
 LANGUAGES_TOP_25 = {
     'en': 'English',
     'es': 'Spanish',
@@ -36,19 +37,28 @@ LANGUAGES_TOP_25 = {
 }
 
 
+# home page where a language can be selected and a movie can be searched 
 def home(request):
     query = request.GET.get('query', '') 
     movies = []
+    # search movies from mongodb database which match the query
     if query:
         movies = get_movies_by_search(query)
     return render(request, 'searchPage.html', {'movies': movies, 'query': query})
 
+
+# recommendation page which shows the list of recommended movies
 def movie_detail(request, movieId):
+    # retrieving the searched movie from the mongodb database
     movie = get_single_movie(movieId)
+
+    # retrieving the selected language
     selected_language = request.GET.get('language', 'en')
 
+    # calling the recommendation algorithm
     matchedMovieIds = likeXbutY(tmdbId=movie['tmdbId'], tmdb_api_key='', originalLanguage=selected_language)
 
+    # retrieving the recommended movies from the mongodb database
     matchedMovies = get_netflix_movies(matchedMovieIds)
 
     return render(request, 'movieDetail.html', {'movie': movie, 'matchedMovies': matchedMovies, 'language': LANGUAGES_TOP_25.get(selected_language)})
